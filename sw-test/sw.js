33a4328ad68761a6ad0e6b276746280a31cbba0a
Amanda Baker <ambake@microsoft.com>
2017-04-27 16:39:02 -0700

    Add all the event listeners

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -1,6 +1,6 @@
-this.addEventListener('install', function(event) {
+this.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('v1').then(function (cache) {
       return cache.addAll([
         '/sw-test/',
         '/sw-test/index.html',
@@ -16,17 +16,37 @@ this.addEventListener('install', function(event) {
   );
 });
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function (event) {
   var response;
-  event.respondWith(caches.match(event.request).catch(function() {
+  event.respondWith(caches.match(event.request).catch(function () {
     return fetch(event.request);
-  }).then(function(r) {
+  }).then(function (r) {
     response = r;
-    caches.open('v1').then(function(cache) {
+    caches.open('v1').then(function (cache) {
       cache.put(event.request, response);
     });
     return response.clone();
-  }).catch(function() {
+  }).catch(function () {
     return caches.match('/sw-test/gallery/myLittleVader.jpg');
   }));
 });
+
+this.addEventListener('statechange', function (event) {
+  console.log('State Changed');
+})
+
+this.addEventListener('controllerchange', function (event) {
+  console.log('Controller Changed');
+})
+
+this.addEventListener('updatefound', function (event) {
+  console.log('Update Found');
+})
+
+this.addEventListener('error', function (event) {
+  console.log('Error' + event.toString());
+})
+
+this.addEventListener('activate', function (event) {
+  console.log('Activated');
+})
