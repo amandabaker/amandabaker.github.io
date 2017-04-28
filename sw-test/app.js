6a6842f77cc2fc8009fc2a85f7eec411f38dcd28
Amanda Baker <ambake@microsoft.com>
2017-04-28 14:04:39 -0700

    Add id to activateEventCounter

--- sw-test/app.js
+++ sw-test/app.js
@@ -99,6 +99,7 @@ window.onload = function () {
   registrations.id = "registrations";
 
   var activateEventCounter = document.createElement('p');
+  activateEventCounter.id = "activateEventCounter";
   activateEventCounter.textContent = `Number of time activate has been triggered: ${activateCount}`;
 
   imgSection.appendChild(unregister);
